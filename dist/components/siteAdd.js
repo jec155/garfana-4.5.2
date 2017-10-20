121e679302d51d364c72020836aa719d630e3654
myfmac <myfmac@myfmacde-MacBook-Pro.local>
2017-10-20 15:09:17 +0800

    黄河入海流

--- dist/components/siteAdd.js
+++ dist/components/siteAdd.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 System.register([], function (_export, _context) {
     "use strict";
@@ -32,39 +32,71 @@ System.register([], function (_export, _context) {
                 };
             }();
 
-            _export('SiteAddCtrl', SiteAddCtrl = function () {
-                function SiteAddCtrl($scope, $http, $location) {
+            _export("SiteAddCtrl", SiteAddCtrl = function () {
+                function SiteAddCtrl($scope, $http, $location, $rootScope) {
                     _classCallCheck(this, SiteAddCtrl);
 
                     this.http = $http;
                     this.scope = $scope;
                     this.location = $location;
-                    $scope.cityModel = {
-                        cityName: '',
-                        cityPingyin: '',
-                        province: '',
-                        country: '中国',
-                        east: 0,
-                        west: 0,
-                        south: 0,
-                        north: 0,
-                        comment: ''
+                    $scope.cityModel = $rootScope.cityModel;
+
+                    $scope.siteMonTypeMap = {
+                        "1": "空气质量",
+                        "2": "空气污染重点企业",
+                        "3": "饮用水水质",
+                        "4": "污水水质",
+                        "5": "水污染重点企业",
+                        "6": "主要流域重点断面水质"
+                    };
+                    $scope.siteTypeMap = {
+                        "PUBLIC": "公有站点",
+                        "PRIVATE": "私有站点"
+                    };
+                    $scope.statusMap = {
+                        "SITE_NORMAL": "正常",
+                        "SITE_DISABLE": "停用",
+                        "SITE_ERROR": "异常"
+                    };
+                    $scope.areaTypeMap = {
+                        "TWO": "适用于居住、商业、工业混杂区"
+                    };
+                    $scope.autoUpdMap = {
+                        true: "是",
+                        false: "否"
                     };
                 }
 
                 _createClass(SiteAddCtrl, [{
-                    key: 'save',
+                    key: "save",
                     value: function save() {
+                        var cityAuto = {
+                            true: 1,
+                            false: 0
+                        };
                         var loc = this.location;
+                        var cityTip = {
+                            "siteCode": this.scope.cityModel.siteCode,
+                            "siteName": this.scope.cityModel.siteName,
+                            "siteType": this.scope.cityModel.siteType,
+                            "siteMontype": this.scope.cityModel.siteMonType,
+                            "cityId": -1,
+                            "createTime": this.scope.cityModel.siteCreateTime,
+                            "siteLatitude": this.scope.cityModel.siteLatitude,
+                            "siteLongitude": this.scope.cityModel.siteLongitude,
+                            "autoUpd": cityAuto[this.scope.cityModel.autoUpd],
+                            "comment": this.scope.cityModel.comment,
+                            "managerment": this.scope.cityModel.managerment };
                         $.ajax({
-                            type: 'POST',
-                            url: 'http://61.164.218.158:8080/AirServer/grafana/addCity',
-                            //'http://127.0.0.1:8080/grafana/addCity',
-                            data: this.scope.cityModel,
+                            type: 'GET',
+                            url: 'http://61.164.218.158:8080/AirServer/grafana/addSite',
+                            //'http://127.0.0.1:8080/grafana/editCity',
+                            data: cityTip,
                             dataType: 'json',
                             success: function success(da) {
+                                //loc.path('plugins/grafana-example-app/page/live-stream');
                                 history.go(-1);
-                                alert('添加成功');
+                                alert('编辑成功');
                             },
                             error: function error(re) {
                                 console.info(re);
@@ -79,7 +111,7 @@ System.register([], function (_export, _context) {
                 return SiteAddCtrl;
             }());
 
-            _export('SiteAddCtrl', SiteAddCtrl);
+            _export("SiteAddCtrl", SiteAddCtrl);
 
             SiteAddCtrl.templateUrl = 'public/plugins/grafana-example-app/components/siteAdd.html';
         }
--- src/components/siteAdd.js
+++ src/components/siteAdd.js
@@ -1,36 +1,69 @@
 
 export class SiteAddCtrl
 {
-  constructor($scope,$http,$location)
+  constructor($scope,$http,$location,$rootScope)
   {
       this.http=$http;
       this.scope=$scope;
       this.location=$location;
-      $scope.cityModel={
-          cityName:'',
-          cityPingyin:'',
-          province:'',
-          country:'中国',
-          east:0,
-          west:0,
-          south:0,
-          north:0,
-          comment:''
-      }
+      $scope.cityModel=$rootScope.cityModel;
+
+      $scope.siteMonTypeMap = {
+            "1" : "空气质量",
+            "2" : "空气污染重点企业",
+            "3" : "饮用水水质",
+            "4" : "污水水质",
+            "5" : "水污染重点企业",
+            "6" : "主要流域重点断面水质"
+        }
+        $scope.siteTypeMap = {
+            "PUBLIC" : "公有站点",
+            "PRIVATE" : "私有站点"
+        }
+        $scope.statusMap = {
+            "SITE_NORMAL" : "正常",
+            "SITE_DISABLE" : "停用",
+            "SITE_ERROR" : "异常"
+        }
+        $scope.areaTypeMap = {
+            "TWO" : "适用于居住、商业、工业混杂区"
+        }
+        $scope.autoUpdMap = {
+            true : "是",
+            false : "否"
+        }
+
   }
   save()
-  {
+  {   
+      let cityAuto={
+        true : 1,
+        false : 0
+      }
       var loc=this.location;
+      let cityTip={
+                        "siteCode":this.scope.cityModel.siteCode,
+                        "siteName":this.scope.cityModel.siteName,
+                        "siteType":this.scope.cityModel.siteType,
+                        "siteMontype":this.scope.cityModel.siteMonType,
+                        "cityId":-1,
+                        "createTime":this.scope.cityModel.siteCreateTime,
+                        "siteLatitude":this.scope.cityModel.siteLatitude,
+                        "siteLongitude":this.scope.cityModel.siteLongitude,
+                        "autoUpd":cityAuto[this.scope.cityModel.autoUpd],
+                        "comment":this.scope.cityModel.comment,
+                        "managerment":this.scope.cityModel.managerment};
       $.ajax({
-          type: 'POST',
-          url: 'http://61.164.218.158:8080/AirServer/grafana/addCity',
-              //'http://127.0.0.1:8080/grafana/addCity',
-          data: this.scope.cityModel,
+          type: 'GET',
+          url: 'http://61.164.218.158:8080/AirServer/grafana/addSite',
+              //'http://127.0.0.1:8080/grafana/editCity',
+          data: cityTip,
           dataType:'json',
           success:function (da)
           {
+              //loc.path('plugins/grafana-example-app/page/live-stream');
               history.go(-1);
-              alert('添加成功');
+              alert('编辑成功');
           },
           error:function (re) {
               console.info(re);
